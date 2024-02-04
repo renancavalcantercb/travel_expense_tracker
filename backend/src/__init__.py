09ea6a09b848c6c027101cbc90fde90e2794efa6
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-02-04 05:30:17 -0300

    feat: added redis start

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -6,6 +6,7 @@ from os import environ
 
 import sys
 import os
+import redis
 
 sys.path.append(os.getcwd())
 
@@ -15,9 +16,13 @@ app = Flask(__name__)
 CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URL")
+app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
+app.config["REDIS_URL"] = environ.get("REDIS_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+
 db = SQLAlchemy(app)
+redis_client = redis.Redis.from_url(app.config["REDIS_URL"])
 
 with app.app_context():
     db.create_all()
