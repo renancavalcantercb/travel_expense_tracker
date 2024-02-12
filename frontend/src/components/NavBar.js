3c7ffdcc39f386f7e3d11faeb439422f852e5ac2
Renan Cavalcante <renancavalcantercb@protonmail.com>
2024-02-12 03:26:14 -0300

    feat: added logout button

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Menu } from 'antd';
-
-const menuItems = [
-    {
-        key: 'home',
-        label: (<Link to="/">Home</Link>),
-    },
-    {
-        key: 'expenses',
-        label: (<Link to="/expenses">Expenses</Link>),
-    },
-    {
-        key: 'exchange',
-        label: (<Link to="/exchange">Exchange</Link>),
-    }
-];
+import { Link, useNavigate } from 'react-router-dom';
+import { Menu, Button, Row, Col } from 'antd';
 
 const Navbar = () => {
+    const navigate = useNavigate();
+
+    const logout = () => {
+        localStorage.clear();
+        navigate('/login'); 
+    };
+
+    const token = localStorage.getItem('token');
+
+    const menuItems = [
+        {
+            key: 'home',
+            label: (<Link to="/">Home</Link>),
+        },
+        {
+            key: 'expenses',
+            label: (<Link to="/expenses">Expenses</Link>),
+        },
+        {
+            key: 'exchange',
+            label: (<Link to="/exchange">Exchange</Link>),
+        }
+    ];
+
     return (
-        <Menu mode="horizontal" items={menuItems} />
+        <Row align="middle" justify="space-between" className='ant-menu-overflow ant-menu ant-menu-root ant-menu-horizontal ant-menu-light custom-menu css-dev-only-do-not-override-1b0bdye'>
+            <Col flex="auto">
+                <Menu mode="horizontal" items={menuItems} className="custom-menu"/>
+            </Col>
+            {token && (
+                <Col>
+                    <Button danger type="primary" onClick={logout}>Logout</Button>
+                </Col>
+            )}
+        </Row>
     );
 };
 
